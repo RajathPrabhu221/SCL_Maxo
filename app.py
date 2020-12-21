0402a2db25bf8c31e068166a9694e4dbed521a67
Gagan-Shenoy <gagan.shenoy@learner.manipal.edu>
2020-12-21 18:28:11 +0530

    Made some changes in comments and cleaned the code

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-
+def validate_signin(email, password):
+    return True
+    
 @app.route('/', methods = ["POST","GET"])
 def home():
     if request.method == "POST":
         user_email = request.form["email"]
         user_password = request.form["pass"]
-        #print("Ayusheer")
-        print(user_email, user_password)
-        return create()
+        #print(user_email, user_password)
+        if validate_signin(user_email, user_password):
+            return create()
+        else:
+            return render_template('index.html')
     else:
         return render_template('index.html')
 
