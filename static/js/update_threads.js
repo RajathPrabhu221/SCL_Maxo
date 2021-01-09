2b0bf6f2ba03f4836a7821c56cc406ba9a3b304b
Gagan-Shenoy <gagan.shenoy@learner.manipal.edu>
2021-01-09 23:11:28 +0530

    added socket functionality to js

---
+++ static/js/update_threads.js
@@ -0,0 +1,58 @@
+var room;
+var a= 1;
+
+$(document).ready(function(){
+
+    function show_hide(){
+        if(a == 1){
+            $('#response').css("display","inline");
+            $('#cancel').css("display","inline");
+            $('#submit').css("display","inline");
+            $('#reply').css("display","none");
+            a = 0;
+        }
+    
+        else{
+            $('#response').css("display","none");
+            $('#cancel').css("display","none");
+            $('#submit').css("display","none");
+            $('#reply').css("display","inline");
+            a = 1;
+        }
+    }
+
+
+    $('#reply').on('click', show_hide);
+    $('#cancel').on('click', show_hide);
+    $('#submit').on('click', show_hide);
+
+    socket = io.connect('http://127.0.0.1:5000')
+
+    socket.emit('join-room', window.location.href);
+    
+    socket.on('connect',function(){
+        console.log('connected');
+    });
+
+    socket.on('joined-room', function(data){
+        room = data;
+    });
+
+    $("#submit").on('click', function(e){
+        e.preventDefault();
+        socket.emit('replied',{ 'thread_content':$('#response').val(), 'thread_room':room });
+        $('#response').val(' ');
+    });
+
+    socket.on('replied',function(data){
+        $('.thread-container').append(
+            `<div class="comment">
+                <div class="user">${data.user}</div>
+                <div class="date">${data.date}</div>
+                <div class="content">
+                    <h5>${data.content}</h5>
+                </div>
+            </div>`);
+    });
+    
+});
